a995418401dac6e7bb0ff1162e8372a9cc012d38
Felix Kahle <kahle.felix@outlook.de>
2024-03-16 02:27:27 +0100

    Update dependencies in .csproj file and added convertTimeBetweenTimezones

---
+++ Cencora.Azure.Timevault/src/Api/ConvertTimeBetweenTimezones.cs
@@ -0,0 +1,106 @@
+// Copyright 2024 Cencora, All rights reserved.
+//
+// Written by Felix Kahle, A123234, felix.kahle@worldcourier.de
+
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Cencora.Azure.Timevault
+{
+    /// <summary>
+    /// Represents a class that converts time between different timezones.
+    /// </summary>
+    public class ConvertTimeBetweenTimezones
+    {
+        /// <summary>
+        /// The logger instance.
+        /// </summary>
+        private readonly ILogger<ConvertTimeBetweenTimezones> _logger;
+
+        /// <summary>
+        /// The timevault service instance.
+        /// </summary>
+        private readonly ITimevaultService _timevaultService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConvertTimeBetweenTimezones"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="timevaultService">The timevault service instance.</param>
+        public ConvertTimeBetweenTimezones(ILogger<ConvertTimeBetweenTimezones> logger, ITimevaultService timevaultService)
+        {
+            _logger = logger;
+            _timevaultService = timevaultService;
+        }
+
+        /// <summary>
+        /// Converts the time between timezones based on the provided location and time.
+        /// </summary>
+        /// <param name="req">The HTTP request containing the location and time parameters.</param>
+        /// <returns>An <see cref="IActionResult"/> representing the result of the conversion.</returns>
+        [Function("convertTimeBetweenTimezones")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            string fromCityString = req.Query["fromCity"].ToString().ToLower();
+            string fromStateString = req.Query["fromState"].ToString().ToLower();
+            string fromPostalCodeString = req.Query["fromPostalCode"].ToString().ToLower();
+            string fromCountryString = req.Query["fromCountry"].ToString().ToLower();
+            string fromTimeString = req.Query["fromTimezone"].ToString();
+
+            string toCityString = req.Query["toCity"].ToString().ToLower();
+            string toStateString = req.Query["toState"].ToString().ToLower();
+            string toPostalCodeString = req.Query["toPostalCode"].ToString().ToLower();
+            string toCountryString = req.Query["toCountry"].ToString().ToLower();
+
+            if (string.IsNullOrEmpty(fromCityString) && string.IsNullOrEmpty(fromStateString) && string.IsNullOrEmpty(fromPostalCodeString) && string.IsNullOrEmpty(fromCountryString))
+            {
+                return new BadRequestObjectResult("At least one of the 'from' location parameters must be provided.");
+            }
+
+            if (string.IsNullOrEmpty(fromTimeString))
+            {
+                return new BadRequestObjectResult("The 'fromTime' parameter must be provided.");
+            }
+
+            if (string.IsNullOrEmpty(toCityString) && string.IsNullOrEmpty(toStateString) && string.IsNullOrEmpty(toPostalCodeString) && string.IsNullOrEmpty(toCountryString))
+            {
+                return new BadRequestObjectResult("At least one of the 'to' location parameters must be provided.");
+            }
+
+            Location fromLocation = new Location(fromCityString, fromStateString, fromPostalCodeString, fromCountryString);
+            Location toLocation = new Location(toCityString, toStateString, toPostalCodeString, toCountryString);
+
+            try
+            {
+                var fromTimezone = await _timevaultService.GetIanaTimezoneAsync(fromLocation);
+                var toTimezone = await _timevaultService.GetIanaTimezoneAsync(toLocation);
+
+                if (string.IsNullOrEmpty(fromTimezone))
+                {
+                    return new NotFoundObjectResult($"No timezone found for the provided location: {fromLocation}");
+                }
+
+                if (string.IsNullOrEmpty(toTimezone))
+                {
+                    return new NotFoundObjectResult($"No timezone found for the provided location: {toLocation}");
+                }
+
+                DateTime dateTime = DateTime.Parse(fromTimeString, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+                TimeZoneInfo sourceTimeZone = TimeZoneInfo.FindSystemTimeZoneById(fromTimezone);
+                TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(toTimezone);
+
+                DateTime convertedDateTime = TimeZoneInfo.ConvertTime(dateTime, sourceTimeZone, targetTimeZone);
+
+                return new OkObjectResult(convertedDateTime);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred while converting the time between timezones: {ex.Message}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
